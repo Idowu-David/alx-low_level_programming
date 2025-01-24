13e7524b4607fe34ee04210ff524339131843e19
David Idowu <idowudavidodun@gmail.com>
2025-01-24 19:13:06 +0100

    Implemented Last digit of a number

---
+++ 1-last_digit.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point: print the last digit of a number
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n, remainder;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	remainder = n % 10;
+	printf("Last digit of %d is %d ", n, remainder);
+	if (n > 5)
+	{
+		printf("and is greater than 5\n");
+	}
+	else if (n == 0)
+	{
+		printf("and is 0\n");
+	}
+	else if (n < 6 && n != 0)
+	{
+		printf("and is less than 6 and not 0\n");
+	}
+
+	return (0);
+}
