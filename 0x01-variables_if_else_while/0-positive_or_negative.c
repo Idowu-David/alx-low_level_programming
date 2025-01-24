8b55909520c17ed55b24f01ca811c3eeee6723d4
David Idowu <idowudavidodun@gmail.com>
2025-01-24 07:19:24 +0100

    Add initial implementation of positive or negative number checker

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point: print if a number is positive or negative
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+
+	if (i < 0)
+	{
+		printf("%d is negative", i);
+	}
+	if (i > 0)
+	{
+		printf("%d is positive", i);
+	}
+	if (i == 0)
+	{
+		printf("%d is zero", i);
+	}
+	printf("\n");
+}
