959f39c797512870e5a1a546ddb97c64b44ab423
David Idowu <idowudavidodun@gmail.com>
2025-02-07 02:57:37 +0100

    print_line

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -2,7 +2,7 @@
 
 /**
  * print_line - draws a straight line in the terminal
- *
+ * @n: number of `-`
  * Return: (void)
  */
 void print_line(int n)
@@ -15,6 +15,6 @@ void print_line(int n)
 	{
 		for (i = 0; i < n; i++)
 			_putchar('_');
+		_putchar('\n');
 	}
-	return;
 }
