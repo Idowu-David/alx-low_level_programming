0a59acf149b7f4db06c8575124629d101a1b05ad
David Idowu <idowudavidodun@gmail.com>
2025-03-15 07:39:29 +0100

    puts2

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses a string
+ * @s: string
+ * Return: nothing, void
+ */
+void rev_string(char *s)
+{
+	int i = 0, start, end;
+
+	while (s[i] != '0')
+		i++;
+	
+}
