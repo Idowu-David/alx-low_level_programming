d1ebbc7f7f6c1f3f153baa7e434d23bf3d4e8a17
David Idowu <idowudavidodun@gmail.com>
2025-03-18 21:08:19 +0100

    cap

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,24 +1,33 @@
 #include "main.h"
 
 /**
- * cap_string - capitalizes all words of a string
- * @str: string
- * Return: a pointer to the new string
+ * cap_string - capitalizes all words of a string.
+ * @str: pointer to strings.
+ *
+ * Return: returns the capitalized string.
  */
 
 char *cap_string(char *str)
 {
-	int len, sep_len, i, j;
-	char separators[] = " \t\n,;.!?\"(){}";
+	int len, sepLen, index, i; /* i means index2 */
+	char *sep = " \t\n,;.!?\"(){}";
 
 	len = strlen(str);
-	sep_len = strlen(separators);
+	sepLen = strlen(sep);
+	/**
+	 * Loop through the separator, another inner loop in the string,
+	 * check if the separator is same as the index of the string
+	 * if true, check the next index of the string
+	 * if the character in the next index is not uppercase
+	 * change to uppercase with 'str[index] + 32
+	 * repeat the process until done.
+	 */
 
-	for (i = 0; i < sep_len; i++)
+	for (index = 0; index < sepLen; index++)
 	{
-		for (j = 0; j < len; j++)
+		for (i = 0; i < len; i++)
 		{
-			if (str[j] == separators[i])
+			if (str[i] == sep[index])
 			{
 				if ((str[i + 1] >= 'a' && str[i + 1] <= 'z'))
 					str[i + 1] -= 32;
