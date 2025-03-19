f7d0c52da9479c80353663e8c030cfef91803b82
David Idowu <idowudavidodun@gmail.com>
2025-03-19 21:58:45 +0100

    condition

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -13,9 +13,7 @@ char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i = 0;
 
-	for (i = 0; i < n && s[i] != '\0'; i++)
+	for (i = 0; i < n; i++)
 		s[i] = b;
-	s[i] = '\0';
-
 	return (s);
 }
