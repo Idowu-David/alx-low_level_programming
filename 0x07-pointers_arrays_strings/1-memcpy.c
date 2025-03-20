a77a859c9c01173a05910aa4f537191bb1b452c2
David Idowu <idowudavidodun@gmail.com>
2025-03-20 14:37:54 +0100

    betty

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -4,6 +4,7 @@
  * _memcpy - copies memory area (n bytes) from src to dest
  * @dest: destination
  * @src: source
+ * @n: bytes
  * Return: a pointer to dest
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
