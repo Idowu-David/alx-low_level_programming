cebb620c458d1a0df529002961e37c7ad084c5fe
David Idowu <idowudavidodun@gmail.com>
2025-03-22 14:51:45 +0100

    betty fix

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -9,13 +9,14 @@ int _sqrt_recursion(int n)
 {
 	if (n < 0)
 		return (-1);
-	return sqrt_helper(n, 0, n);
+	return (sqrt_helper(n, 0, n));
 }
 
 /**
  * sqrt_helper - performs binary search to find the integer square root
  * @low: lowest range
  * @high: highest range
+ * @n: number
  * Return: square root
  */
 
@@ -25,11 +26,11 @@ int sqrt_helper(int n, int low, int high)
 	int midSq = mid * mid;
 
 	if (low > high)
-		return high;
+		return (high);
 	if (midSq == n)
-		return mid;
+		return (mid);
 	else if (midSq < n)
-		return sqrt_helper(n, mid + 1, high);
+		return (sqrt_helper(n, mid + 1, high));
 	else
-		return sqrt_helper(n, low, mid - 1);
+		return (sqrt_helper(n, low, mid - 1));
 }
