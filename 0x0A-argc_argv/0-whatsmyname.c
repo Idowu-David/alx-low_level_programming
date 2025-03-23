6bef92da39e19407014ff15ce49329929a0b5914
David Idowu <idowudavidodun@gmail.com>
2025-03-23 12:34:12 +0100

    my name is

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - prints its name
+ * @argc: number of arguments
+ * @argv: pointer to array of arguments passed
+ * Return: 0, success
+ */
+int main(int argc, char **argv)
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
