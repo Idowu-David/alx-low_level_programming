0c65c1f40b93e31f205a53a197d679f09ff3f453
David Idowu <idowudavidodun@gmail.com>
2025-03-24 11:33:06 +0100

    +1 space

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -16,7 +16,7 @@ char *_strdup(char *str)
 
 	while (str[len] != '\0')
 		len++;
-	mem = malloc(len * (sizeof(char)));
+	mem = malloc((len + 1) * (sizeof(char)));
 	if (mem == NULL)
 		return (NULL);
 	for (i = 0; i < len; i++)
