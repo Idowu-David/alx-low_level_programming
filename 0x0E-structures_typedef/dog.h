47d0c921f9ae2d7be2978aa01953d19cd2231a86
David Idowu <idowudavidodun@gmail.com>
2025-03-26 14:13:03 +0100

    print dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -16,8 +16,10 @@ struct dog
 	float age;
 };
 
+#include <stdlib.h>
+#include <stdio.h>
 void init_dog(struct dog *d, char *name, float age, char *owner);
-
+void print_dog(struct dog *d);
 
 
 #endif
