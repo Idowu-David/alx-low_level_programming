c359c2ab915a013f085b24eb2b1fc223e71dc840
David Idowu <idowudavidodun@gmail.com>
2025-03-28 23:11:29 +0100

    fix

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -9,9 +9,5 @@ int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char *const format, ...);
-typedef char c;
-typedef int i;
-typedef float;
-typedef char *s;
 
 #endif
