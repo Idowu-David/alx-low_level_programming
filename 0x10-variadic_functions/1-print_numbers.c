d216a0ee5c97e3296219b96a9da1b6e6451b6851
David Idowu <idowudavidodun@gmail.com>
2025-03-28 17:30:17 +0100

    separator

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -2,7 +2,7 @@
 
 /**
  * print_numbers - prints numbers
- * @separator = string to be printed between numbers
+ * @separator: string to be printed between numbers
  * @n: number of integers passed to the function
  * Return: nothing, void
  */
@@ -11,10 +11,15 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_list args;
 	unsigned int i;
 
+	if (n == 0)
+		return;
+	if (separator == NULL)
+		separator = "";
 	va_start(args, n);
 	for (i = 0; i < n; i++)
 	{
 		if (i < (n - 1))
+
 			printf("%d%s", va_arg(args, int), separator);
 		else
 			printf("%d\n", va_arg(args, int));
