4748539ef66ebdc4864e9c75477f5d7d429383ec
David Idowu <idowudavidodun@gmail.com>
2025-03-29 22:10:58 +0100

    len

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -9,11 +9,14 @@
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *node;
+	int len = 0;
 
 	node = malloc(sizeof(list_t));
 	if (node == NULL)
 		return (NULL);
-	node->len = strlen(str);
+	while (str[len] != '\0')
+		len++;
+	node->len = len;
 	node->str = strdup(str);
 	node->next = NULL;
 	if (head != NULL)
