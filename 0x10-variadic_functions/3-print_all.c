3768e1f6917356812f9bd2124193c931e8ccb46e
David Idowu <idowudavidodun@gmail.com>
2025-03-29 14:16:32 +0100

    print_all

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -7,9 +7,41 @@
  */
 void print_all(const char *const format, ...)
 {
-	int len = strlen(format);
 	va_list args;
+	int format_index = 0;
+	char *str = (char *)format;
+	char *separator = "";
 
-	va_start(args, len);
+	va_start(args, format);
+	while (str && str[format_index])
+	{
+		switch (str[format_index])
+		{
+		case 'c':
+			printf("%s%c", separator, va_arg(args, int));
+			break;
+		case 'i':
+			printf("%s%d", separator, va_arg(args, int));
+			break;
+		case 'f':
+			printf("%s%f", separator, va_arg(args, double));
+			break;
+		case 's':
+			{
+				char *s_val = va_arg(args, char *);
 
+				if (!s_val)
+					s_val = "(nil)";
+				printf("%s%s", separator, s_val);
+				break;
+			}
+		default:
+			format_index++;
+			continue;
+		}
+		separator = ", ";
+		format_index++;
+	}
+	va_end(args);
+	printf("\n");
 }
