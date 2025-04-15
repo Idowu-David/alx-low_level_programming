cf416edddc728dffb04db73e6ccd7da0dfe521a0
David Idowu <idowudavidodun@gmail.com>
2025-04-15 15:44:39 +0100

    all bit project

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,29 +1,29 @@
 #include "main.h"
 
 /**
- * binary_to_uint - converts a binary number to an unsigned int
- * @b: pointer to a string of 0 and 1 chars
- * Return: the converted number or 0 if
- * - there is one or more chars in the string b that is not 0 or 1
+ * binary_to_uint - converts a binary number to an unsigned int.
+ * @b: pointer to string of binary number.
+ *
+ * Return: the converted number, or 0 if
+ * - there is one or more chars in the string b that is not 0 or 1.
  * - b is NULL
  */
+
 unsigned int binary_to_uint(const char *b)
 {
-	int i = 0;
-	unsigned int result = 0;
+	unsigned int decimal = 0;
+	int i;
 
-	while (b[i] != '\0')
+	if (b == NULL)
+		return 0;
+
+	for (i = 0; b[i] != '\0'; i++)
 	{
-		result <<= 1;
-		if (b[i] == '1')
-			result |= 1;
-		else if (b[i] != '1' && b[i] != '0')
-		{
-			result = 0;
-			break;
-		}
+		if (b[i] != '0' && b[i] != '1')
+			return 0;
 
-		i++;
+		decimal = decimal * 2 + (b[i] - '0');
 	}
-	return (result);
+
+	return decimal;
 }
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,9 +1,22 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+/* Standard Libraries */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stddef.h>
 
-
+/* Mandatory tasks function prototypes */
+int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
+void print_binary(unsigned long int n);
+int get_bit(unsigned long int n, unsigned int index);
+int set_bit(unsigned long int *n, unsigned int index);
+int clear_bit(unsigned long int *n, unsigned int index);
+unsigned int flip_bits(unsigned long int n, unsigned long int m);
+int get_endianness(void);
 
-
+/* My function prototype */
+int binary_int_check(const char *str);
 #endif
