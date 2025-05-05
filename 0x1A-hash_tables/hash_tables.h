d431e16f4bdd8beb84e11d08ebb5cec5049224b8
David Idowu <idowudavidodun@gmail.com>
2025-05-05 14:41:37 +0100

    index

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -38,4 +38,7 @@ typedef struct hash_table_s
 int _putchar(char c);
 hash_table_t *hash_table_create(unsigned long int size);
 unsigned long int hash_djb2(const unsigned char *str);
+unsigned long int key_index(const unsigned char *key, unsigned long int size);
+
+
 #endif
