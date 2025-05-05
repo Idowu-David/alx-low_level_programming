4af5dfc40ecf2230775ef9b5ba5fd23db4b7d0d8
David Idowu <idowudavidodun@gmail.com>
2025-05-05 15:43:01 +0100

    hash table set

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,58 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - adds an element to the hash table
+ * @ht: a pointer to the hash table
+ * @key: the key
+ * @value: value associated with the key
+ * Return: 1 if success, 0 otherwise
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	hash_node_t *node, *head, *ptr;
+	unsigned long int index;
+
+	if (ht == NULL || key == NULL || *key == '\0')
+		return (0);
+	node = malloc(sizeof(hash_node_t));
+	if (node == NULL)
+		return (0);
+	node->key = strdup(key);
+	if (node->key == NULL)
+	{
+		free(node);
+		return (0);
+	}
+	node->value = strdup(value);
+	if (node->value == NULL)
+	{
+		free(node->key);
+		free(node);
+		return (0);
+	}
+	node->next = NULL;
+
+	index = key_index((const unsigned char *)key, ht->size);
+	head = ht->array[index];
+	if (head == NULL)
+		head = node;
+	else
+	{
+		ptr = head;
+		while (ptr != NULL)
+		{
+			if (strcmp(ptr->key, key) == 0)
+			{
+				free(ptr->value);
+				ptr->value = strdup(value);
+				if (ptr->value == NULL)
+					return (0);
+				return (1);
+			}
+			ptr = ptr->next;
+		}
+		node->next = ht->array[index];
+		ht->array[index] = node;
+	}
+	return (1);
+}
--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -4,7 +4,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stddef.h>
-
+#include <string.h>
 
 /**
  * struct hash_node_s - Node of a hash table
@@ -39,6 +39,7 @@ int _putchar(char c);
 hash_table_t *hash_table_create(unsigned long int size);
 unsigned long int hash_djb2(const unsigned char *str);
 unsigned long int key_index(const unsigned char *key, unsigned long int size);
+int hash_table_set(hash_table_t *ht, const char *key, const char *value);
 
 
 #endif
