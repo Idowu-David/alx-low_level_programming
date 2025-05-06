6fedfd014e9bff28e8e0f268ddbce28bb223653b
David Idowu <idowudavidodun@gmail.com>
2025-05-06 15:36:04 +0100

    hash table set

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -14,46 +14,37 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	if (ht == NULL || key == NULL || *key == '\0')
 		return (0);
+	index = key_index((const unsigned char *)key, ht->size);
+	head = ht->array[index];
+
+	while (head != NULL)
+	{
+		if (strcmp(head->key, key) == 0) /* update value of key if exists */
+		{
+			free(head->value);
+			head->value = strdup(value);
+			if (head->value == NULL)
+				return (0);
+			return (1);
+		}
+		head = head->next;
+	}
+
 	node = malloc(sizeof(hash_node_t));
 	if (node == NULL)
 		return (0);
+
 	node->key = strdup(key);
-	if (node->key == NULL)
-	{
-		free(node);
-		return (0);
-	}
 	node->value = strdup(value);
-	if (node->value == NULL)
+	if (node->key == NULL || node->value == NULL)
 	{
 		free(node->key);
+		free(node->value);
 		free(node);
 		return (0);
 	}
-	node->next = NULL;
+	node->next = ht->array[index];
+	ht->array[index] = node;
 
-	index = key_index((const unsigned char *)key, ht->size);
-	head = ht->array[index];
-	if (head == NULL)
-		ht->array[index] = node;
-
-	else
-	{
-		ptr = head;
-		while (ptr != NULL)
-		{
-			if (strcmp(ptr->key, key) == 0)
-			{
-				free(ptr->value);
-				ptr->value = strdup(value);
-				if (ptr->value == NULL)
-					return (0);
-				return (1);
-			}
-			ptr = ptr->next;
-		}
-		node->next = ht->array[index];
-		ht->array[index] = node;
-	}
 	return (1);
 }
