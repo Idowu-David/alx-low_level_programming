fe21e3e593bae70071648bca0f2f985da574a015
David Idowu <idowudavidodun@gmail.com>
2025-05-06 16:28:53 +0100

    hash table print

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,31 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - prints a hash table
+ * @ht: hash table
+ * Return: nothing, void
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	hash_node_t *node;
+	unsigned long int i;
+	int printed = 0;
+
+	if (ht == NULL)
+		return;
+
+	printf("{");
+	for (i = 0; i < ht->size; i++)
+	{
+		node = ht->array[i];
+		while (node != NULL)
+		{
+			if (printed)
+				printf(", ");
+			printf("'%s': '%s'", node->key, node->value);
+			printed = 1;
+			node = node->next;
+		}
+	}
+	printf("}\n");
+}
